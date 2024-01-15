c9c4a78d3b95b2fa25cee4ba60b82ff3008c8534
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-01-15 21:07:40 +0500

    sidebar posiiton fixed and setting alignments and heights

--- adminlte-clone/script.js
+++ adminlte-clone/script.js
@@ -1,7 +1,9 @@
+// variables
 const menuBtn = document.querySelector('.sidebar-btn');
-const sdiebar = document.querySelector('.sidebar');
+const sidebar = document.querySelector('.sidebar');
 const sidebarSpan = document.querySelectorAll('#sidebar span');
 const sidebarInput = document.querySelector('#sidebar .input-group');
+const main = document.querySelector('.main');
 
 // add span display:block class to all sidebar's span
 sidebarSpan.forEach((span)=>{
@@ -9,10 +11,46 @@ sidebarSpan.forEach((span)=>{
 });
 
 menuBtn.addEventListener('click',()=>{
-    sdiebar.classList.toggle('sidebar-toggle');
+    sidebar.classList.toggle('sidebar-toggle');
+    console.log(sidebar.className);
+    // console.log(sidebar.offsetWidth);
+
+    // main.style.marginLeft = sidebar.offsetWidth;
+    if (sidebar.offsetWidth == 246) {
+        console.log('246');
+        main.style.marginLeft = '70px';
+    }else if (sidebar.offsetWidth == 70) {
+        console.log('70');
+        main.style.marginLeft = '240px';
+
+    }
+
     sidebarSpan.forEach((span)=>{
         span.classList.toggle('span-none');
+
     });
     sidebarInput.classList.toggle('span-none');
-    
+
 });
+
+    
+    // mouse enter
+    sidebar.addEventListener('mouseenter',()=>{
+        if (sidebar.offsetWidth == 70) {
+
+            sidebarSpan.forEach((span)=>{
+                span.classList.remove('span-none');
+            });
+        }
+    });
+    // mouse leave
+    sidebar.addEventListener('mouseleave',()=>{
+        console.log('mouse leave');
+        if (sidebar.offsetWidth == 246) {
+
+            sidebarSpan.forEach((span)=>{
+                span.classList.add('span-none');
+            });
+        }
+    
+    });
