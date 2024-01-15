39be5f277cc58f58ef3f8801f1908c46ef3d5f6b
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-01-15 22:42:48 +0500

    sidebar header issue fixed

--- adminlte-clone/script.js
+++ adminlte-clone/script.js
@@ -4,7 +4,6 @@ const sidebar = document.querySelector('.sidebar');
 const sidebarSpan = document.querySelectorAll('#sidebar span');
 const sidebarInput = document.querySelector('#sidebar .input-group');
 const main = document.querySelector('.main');
-
 // add span display:block class to all sidebar's span
 sidebarSpan.forEach((span)=>{
     span.classList.add('span');
@@ -16,14 +15,16 @@ menuBtn.addEventListener('click',()=>{
     // console.log(sidebar.offsetWidth);
 
     // main.style.marginLeft = sidebar.offsetWidth;
-    if (sidebar.offsetWidth == 246) {
-        console.log('246');
-        main.style.marginLeft = '70px';
-    }else if (sidebar.offsetWidth == 70) {
-        console.log('70');
-        main.style.marginLeft = '240px';
+        if (sidebar.offsetWidth == 246) {
+            console.log('246');
+            main.style.marginLeft = '70px';
+        }else if (sidebar.offsetWidth == 70) {
+            console.log('70');
+            main.style.marginLeft = '240px';
+    
+        }
 
-    }
+       
 
     sidebarSpan.forEach((span)=>{
         span.classList.toggle('span-none');
@@ -41,15 +42,26 @@ menuBtn.addEventListener('click',()=>{
             sidebarSpan.forEach((span)=>{
                 span.classList.remove('span-none');
             });
+
+            sidebar.addEventListener('mouseleave',()=>{
+                console.log('mouse leave');
+                if (sidebar.offsetWidth == 246) {
+        
+                    sidebarSpan.forEach((span)=>{
+                        span.classList.add('span-none');
+                    });
+                }
+            
+            });
         }
     });
     // mouse leave
     sidebar.addEventListener('mouseleave',()=>{
         console.log('mouse leave');
-        if (sidebar.offsetWidth == 246) {
+        if (sidebar.offsetWidth == 70) {
 
             sidebarSpan.forEach((span)=>{
-                span.classList.add('span-none');
+                // span.classList.add('span-none');
             });
         }
     
