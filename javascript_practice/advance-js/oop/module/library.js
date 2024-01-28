c102439cdc77e699bd18657864685c91a3a8b58d
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-01-28 23:01:23 +0500

    modules in js

--- javascript_practice/advance-js/oop/module/library.js
+++ javascript_practice/advance-js/oop/module/library.js
@@ -1,1 +1,11 @@
-export let name = 'LoneWolf';
+let name = 'LoneWolf';
+function user(userN){
+    console.log(`Hye ${userN} I am CEO ${name}`);
+}
+
+function salaryBonus(n1,n2){
+    return n1 + n2;
+}
+
+// shorthand for export 
+export {name, user, salaryBonus}
--- javascript_practice/advance-js/oop/module/main.js
+++ javascript_practice/advance-js/oop/module/main.js
@@ -1,2 +1,12 @@
-import {name} from './library.js';
+import {name, user, salaryBonus as bonus} from './library.js';
+import * as mgN from './name-list-module.js';
+
+console.log(mgN);  //expected outuput Module{....}
+console.log(mgN.manager1 + " is absent");
 console.log(name);
+
+// function from library.js
+user('Saqib');
+// function that will return something
+let bonusOfTwoPeople = bonus(1000,1000);
+console.log(bonusOfTwoPeople);
