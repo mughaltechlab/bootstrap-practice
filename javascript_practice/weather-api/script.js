c81dc0c46b8335d54c75e1226405a429ed45012b
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-02-04 14:58:01 +0500

    weather app finished

--- javascript_practice/weather-api/script.js
+++ javascript_practice/weather-api/script.js
@@ -7,6 +7,7 @@ const cityInp = document.querySelector('#cityInp');
 const icon = document.querySelector('#icon');
 const okBtn = document.querySelector('#submit-btn');
 const description = document.querySelector('#description');
+const temperature = document.querySelector('#temp');
 
 
 // event listener on OKBTN
@@ -20,7 +21,7 @@ okBtn.addEventListener('click',()=>{
 async function getData(value){
     try {
         //* fetch url
-        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${apiKey}`);
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${apiKey}&units=metric`);
         //*   check response status if not ok 
        if (!res.ok) {
            console.log('error');
@@ -30,17 +31,20 @@ async function getData(value){
             const data = await res.json();
 
             // data variables
-            ic = data.weather[0].icon;
-            des = data.weather[0].description;
+            const ic = data.weather[0].icon;
+            const des = data.weather[0].description;
+            const temp = Math.round(data.main.temp);
 
             
             //    set elements in html
             // set icon
             icon.innerHTML = `<img src="https://openweathermap.org/img/wn/${ic}@2x.png" alt="weather-icon">`;
-            console.log(ic);
             // set description
             description.innerHTML = des;
-            console.log(des);
+            // set temperature
+            temperature.innerHTML = temp;
+            console.log(temp);
+            console.log(data);
 
 
        }
